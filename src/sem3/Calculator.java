54bf3a6564cdb1751eb7320fa6ad87433b8247d7
Margarita <savmargo98@gmail.com>
2024-03-31 22:00:55 +0800

    3

---
+++ src/sem3/Calculator.java
@@ -0,0 +1,29 @@
+package sem3;
+/*
+Написать класс Калькулятор (необобщенный), который содержит обобщенные статические
+методы: sum(), multiply(), divide(), subtract(). Параметры этих методов – два числа разного
+типа, над которыми должна быть произведена операция. Методы должны возвращать результат своей работы.
+ */
+
+
+public class Calculator {
+
+     public static <T extends Number> double sum(T t1, T t2) {
+          return t1.doubleValue() + t2.doubleValue();
+
+     }
+    public static <T extends Number> double multiply(T t1, T t2) {
+        return t1.doubleValue() * t2.doubleValue();
+
+    }
+    public static <T extends Number> double divide(T t1, T t2) {
+        return t1.doubleValue() / t2.doubleValue();
+
+    }
+    public static <T extends Number> double subtract(T t1, T t2) {
+        return t1.doubleValue() - t2.doubleValue();
+
+    }
+
+
+}
---
+++ src/sem3/Main.java
@@ -0,0 +1,8 @@
+package sem3;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.subtract(15.34,16.2335));
+    }
+}
